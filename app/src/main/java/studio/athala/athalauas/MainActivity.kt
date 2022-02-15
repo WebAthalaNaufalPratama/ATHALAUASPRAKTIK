caf105a988fabf7d39df1eb22c24a7c63c0703d3
Athala Naufal Pratama <Attagledek546@gmail.com>
2022-02-15 23:18:05 +0700

    Initial commit

---
+++ app/src/main/java/studio/athala/athalauas/MainActivity.kt
@@ -0,0 +1,21 @@
+package studio.athala.athalauas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var container : LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        container = findViewById(R.id.container)
+        container.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
